bd7aece9560590b78dc25861410f30370d52f3ec
PatrickStar <1592810043@qq.com>
2023-01-09 19:22:17 +0800

    init

---
+++ src/main/java/team/delete/PurseBuddy/PurseBuddyApplication.java
@@ -0,0 +1,13 @@
+package team.delete.PurseBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurseBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurseBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/delete/PurseBuddy/PurseBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package team.delete.PurseBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurseBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
